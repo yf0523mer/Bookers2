d7ad0d7d1f6ecda1fcaeb85a843e2c3e43c1f6a9
yf0523mer <fuku0118yf@gmail.com>
2019-03-18 09:05:20 +0000

    reset

--- app/models/book.rb
+++
@@ -1,6 +0,0 @@
-class Book < ApplicationRecord
-	belongs_to :user
-	validates :title, presence: true
-	validates :opinion, presence: true
-	validates :opinion, length: { maximum: 200 }
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'home/about'
-  devise_for :users
-  root 'users#top', as: 'top_user'
-  get "home/about" => "home#about"
-  resources :books
-  resources :users, only: [:index, :show, :edit, :update, :destroy, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
